a38e9a767eba54e669f6a8ff6d4aca97ccc4a35d
Mohammad Samiul Kabir <samiulkabir0522@gmail.com>
2025-10-18 23:20:49 +0600

    Add Game class and main function to display games

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+class Game{
+  public:
+    string name;
+    float gb;
 
+    Game(string name, float gb){
+      this->name=name;
+      this->gb=gb;
+    }
+};
+int main(){
+  Game part1("OTG",56);
+  Game part2 ("Valo",46);
+
+  cout<<part1.name<<" "<<part1.gb<<endl;
+  cout<<part2.name<<" "<<part2.gb<<endl;
+  
+  
+
+}
